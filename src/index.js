36bbb9567550569a5fdf3e5f34d88deae2e47d8a
xy <sagexp@163.com>
2019-11-12 11:08:58 +0800

    remove warning

--- src/index.js
+++ src/index.js
@@ -137,7 +137,7 @@ const Swipeout = createReactClass({
     };
   },
 
-  componentWillMount: function () {
+  componentDidMount: function () {
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (event, gestureState) => true,
       onStartShouldSetPanResponderCapture: (event, gestureState) =>
@@ -154,11 +154,11 @@ const Swipeout = createReactClass({
     });
   },
 
-  componentWillReceiveProps: function (nextProps) {
-    if (nextProps.close) this._close();
-    if (nextProps.openRight) this._openRight();
-    if (nextProps.openLeft) this._openLeft();
-  },
+  componentDidUpdate (prevProps, prevState) {
+    if (this.props.close) this._close();
+    if (this.props.openRight) this._openRight();
+    if (this.props.openLeft) this._openLeft();
+  }
 
   _handlePanResponderGrant: function (e: Object, gestureState: Object) {
     if (this.props.disabled) return;
